dc5cbcd62fa683810bf067d5af1f5927b99dc9f8
Алексей <94749564+aleksejtalanov13@users.noreply.github.com>
2024-04-18 12:11:15 +0500

    Update init.bat

--- init.bat
+++ init.bat
@@ -7,7 +7,7 @@ cd C:\lifeserv\
 xcopy C:\Users C:\lifeserv\players /s /y
 xcopy C:\lifeserv\players C:\lifeserv\world /s /y
 echo Thats my world > C:\lifeserv\world\readme.txt
-cd C:\hacklife\newlife
+cd C:\hacklife\newlife\7z
 7za a world.7z C:\lifeserv\world
 copy world.7z C:\hacklife\
 exit
